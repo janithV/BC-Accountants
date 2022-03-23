c42a2c4168c29deb466aeb4c7ab99811695f6bfc
Nipuna Muansinghe <mnipunai7@gmail.com>
2022-03-23 20:28:43 +0530

    Styling for Home Banner1

--- src/Components/Banner1/index.js
+++ src/Components/Banner1/index.js
@@ -1,11 +1,15 @@
 import React from "react"
 import { Parallax } from "react-parallax";
+import styles from "./style";
 
 const Banner1 = ({image, text, button}) => {
     return( 
         <Parallax style={{paddingTop:60}} bgImage={image}>
-            <div style={{height:500,alignItems:"center"}} className="d-flex bd-highlight mb-3 example-parent">{text}
-            <button className="align-self-center">{button}</button>
+            <div style={{height:500}}>
+                <div style={styles.banner1} className="d-flex bd-highlight mb-3 example-parent">
+                    <div>{text} {"\n"}</div>
+                  <div><button className="align-self-center">{button}</button></div>
+                </div>
             </div>
         </Parallax>
     );
---
+++ src/components/Banner1/style.js
@@ -0,0 +1,15 @@
+
+const styles = ({
+    banner1: {
+        padding: 20,
+        top: '50%',
+        left: '50%',
+        position: 'absolute',
+        transform: 'translate(-50%, -50%)',
+        backgroundColor:'#fff',
+        alignItems: 'center',
+        flexDirection: 'column'
+    }
+});
+
+export default styles;
