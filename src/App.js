27df26b2f065bb6644696b11a3c0f5f850f5f9ef
janithV <55439779+janithV@users.noreply.github.com>
2022-03-30 15:21:31 +0530

    routing added, navbar updated
    need some new css changes

--- src/App.js
+++ src/App.js
@@ -1,51 +1,22 @@
-import logo from "./bclogonew.png";
 import "./App.css";
 import React from "react";
+import {Route,Switch,Redirect} from 'react-router-dom'
 import Home from "./Screens/Home";
+import NavBar from "./components/NavBar/navbar";
+import NotFound from './components/NotFound/notfound'
 import ReturnDetails from "./components/FormComponents/ReturnDetails";
 
 function App() {  
   return (
     <React.Fragment>
-      <header>
-        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-              <img src={logo} alt="" width="30" height="24"></img>
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarCollapse"
-              aria-controls="navbarCollapse"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarCollapse">
-              <ul className="navbar-nav me-auto mb-2 mb-md-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Link
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link disabled">Disabled</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </header>
-      <main className="flex-shrink-0"><Home /></main>
-      {/* <main className="flex-shrink-0"><ReturnDetails /></main> */}
+      <NavBar/>
+      <Switch>
+        <Route path='/individuals' component={ReturnDetails}/>
+        <Route path='/notfound' component={NotFound}/>
+        <Route path='/' exact component={Home}/>
+        <Redirect to='/notfound'/>
+      </Switch>
+     
       
     </React.Fragment>
   );
---
+++ src/components/NavBar/navbar.js
@@ -0,0 +1,36 @@
+import logo from "./bclogonew.png";
+import {Link} from 'react-router-dom'
+import { Container, Navbar,Nav,NavDropdown } from "react-bootstrap";
+const NavBar = () => {
+    return (       
+    <Navbar bg="dark" variant="dark" expand="lg">
+    <Container>
+    <Navbar.Brand href="#home">
+    <img
+          alt=""
+          src={logo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+      BC Accountants</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link><Link to='/'>Home</Link></Nav.Link>
+        <NavDropdown title="Online Returns" id="basic-nav-dropdown">
+          <NavDropdown.ItemText ><b>Business</b></NavDropdown.ItemText>
+          <NavDropdown.Item href="#action/3.2">Something</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.ItemText><b>Individuals</b></NavDropdown.ItemText>
+          <NavDropdown.Item><Link to='/individuals'>Individuals</Link></NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+      );
+}
+ 
+export default NavBar;
