f47025224e21c9b17eec4047fac6d6b93acec502
Nipuna Muansinghe <mnipunai7@gmail.com>
2022-03-30 22:03:31 +0530

    Pretty code

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -3,7 +3,7 @@ import { Table, Column, HeaderCell, Cell } from 'rsuite-table';
 import 'rsuite-table/dist/css/rsuite-table.css';
 
 function taxTable({content,type,keys}) {
-  
+
   const colsType2 = ['col1','col2','col3','col4','col5','col6','col7'];
   const type1Width = [180,150,150,400];
   const type2Width =[250,110,110,110,110,110,110]
@@ -20,21 +20,23 @@ function taxTable({content,type,keys}) {
         >
           {type ==='type1' && keys.map((key, index) => (
               <>
-              <Column width={type1Width[index]} colSpan={2}>
-              <HeaderCell>{key}</HeaderCell>
-              <Cell dataKey={colsType2[index]} rowIndex={index} key={index}/>
-            </Column>
-           </>
+                <Column width={type1Width[index]} colSpan={2}>
+                  <HeaderCell>{key}</HeaderCell>
+                  <Cell dataKey={colsType2[index]} rowIndex={index} key={index}/>
+                </Column>
+              </>
             ))              
             }
+            
             {type ==='type2' && keys.map((key, index) => (
               <>
-              <Column width={type2Width[index]} colSpan={6}>
-              <HeaderCell>{key}</HeaderCell>
-              <Cell dataKey={colsType2[index]} rowIndex={index} key={index}/>
-            </Column>
-           </>
-            ))              }
+                <Column width={type2Width[index]} colSpan={6}>
+                  <HeaderCell>{key}</HeaderCell>
+                  <Cell dataKey={colsType2[index]} rowIndex={index} key={index}/>
+                </Column>
+              </>
+            ))              
+            }
         </Table>
       
   </div>
