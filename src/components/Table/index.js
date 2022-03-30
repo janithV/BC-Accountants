013663885fed35729e4eb81cbfcae55e7799b22b
Nipuna Muansinghe <mnipunai7@gmail.com>
2022-03-30 22:01:22 +0530

    Table card update

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -3,10 +3,10 @@ import { Table, Column, HeaderCell, Cell } from 'rsuite-table';
 import 'rsuite-table/dist/css/rsuite-table.css';
 
 function taxTable({content,type,keys}) {
-  const colsType1 = ['col1','col2','col3','col4'];
+  
   const colsType2 = ['col1','col2','col3','col4','col5','col6','col7'];
   const type1Width = [180,150,150,400];
-  const type2Width =[250,100,100,100,100,100,100]
+  const type2Width =[250,110,110,110,110,110,110]
 
   return (
     <div> 
@@ -22,7 +22,7 @@ function taxTable({content,type,keys}) {
               <>
               <Column width={type1Width[index]} colSpan={2}>
               <HeaderCell>{key}</HeaderCell>
-              <Cell dataKey={colsType1[index]} rowIndex={index} key={index}/>
+              <Cell dataKey={colsType2[index]} rowIndex={index} key={index}/>
             </Column>
            </>
             ))              
