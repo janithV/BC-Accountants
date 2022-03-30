5cc001eda3a69d2d0926822fc6a5587f12eec93b
Nipuna Muansinghe <mnipunai7@gmail.com>
2022-03-30 05:47:07 +0530

    CAPITAL 2 simple

--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { useState } from "react";
-import Cards from "../../Components/Cards/index";
+import Cards from "../../components/Cards/index";
 import parallaxImage from "../../Assets/Images/parallaxImage.jpg";
 import calendarGif from "../../Assets/Images/calendar.gif"
-import Banner1 from "../../Components/Banner1";
+import Banner1 from "../../components/Banner1";
 import styles from "./style";
 import Banner2 from "../../components/Banner2";
 import Table from "../../components/Table";
